dfdbf2918711bc592cb863adf6e96e5212f94380
Vivek <ajay.g@jayskeralaenterprises.com>
2025-10-13 09:40:58 +0530

    Fix production environment variables for proxy-session-data endpoint

--- Server/ecosystem.config.js
+++ Server/ecosystem.config.js
@@ -10,15 +10,13 @@ export default {
       NODE_ENV: 'development',
       PORT: 5000,
       AI_ROUTE_API: 'https://api.jayskeralainnovations.com/flask1',
-      AI_ROUTE_API_SECOND: 'https://api.jayskeralainnovations.com/flask1',
-      JWT_ACCESS_SECRET: '60d381821fbd976622eacb5ce81518b42288fbce460a57fcb9be714e4b35ad7438c578b19ae6971900b8c4a7dd8aa09f9366fc5243936e8735cf014aee580b1b'
+      AI_ROUTE_API_SECOND: 'https://api.jayskeralainnovations.com/flask1'
     },
     env_production: {
       NODE_ENV: 'production',
       PORT: 5000,
       AI_ROUTE_API: process.env.AI_ROUTE_API || 'https://api.jayskeralainnovations.com/flask1',
-      AI_ROUTE_API_SECOND: process.env.AI_ROUTE_API_SECOND || 'https://api.jayskeralainnovations.com/flask1',
-      JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET || '60d381821fbd976622eacb5ce81518b42288fbce460a57fcb9be714e4b35ad7438c578b19ae6971900b8c4a7dd8aa09f9366fc5243936e8735cf014aee580b1b'
+      AI_ROUTE_API_SECOND: process.env.AI_ROUTE_API_SECOND || 'https://api.jayskeralainnovations.com/flask1'
     }
   }]
 };
