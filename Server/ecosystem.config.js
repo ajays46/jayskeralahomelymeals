53fb84fc1aa6b00a736d84e4ad9671f3fd033b1e
Vivek <ajay.g@jayskeralaenterprises.com>
2025-12-22 12:44:07 +0530

    added ecosystem config

--- Server/ecosystem.config.js
+++ Server/ecosystem.config.js
@@ -10,13 +10,15 @@ export default {
       NODE_ENV: 'development',
       PORT: 5000,
       AI_ROUTE_API: 'https://api.jayskeralainnovations.com/flask1',
-      AI_ROUTE_API_SECOND: 'https://api.jayskeralainnovations.com/flask1'
+      AI_ROUTE_API_SECOND: 'https://api.jayskeralainnovations.com/flask2',
+      AI_ROUTE_API_THIRD: 'https://api.jayskeralainnovations.com/flask3'
     },
     env_production: {
       NODE_ENV: 'production',
       PORT: 5000,
       AI_ROUTE_API: process.env.AI_ROUTE_API || 'https://api.jayskeralainnovations.com/flask1',
-      AI_ROUTE_API_SECOND: process.env.AI_ROUTE_API_SECOND || 'https://api.jayskeralainnovations.com/flask1'
+      AI_ROUTE_API_SECOND: process.env.AI_ROUTE_API_SECOND || 'https://api.jayskeralainnovations.com/flask2',
+      AI_ROUTE_API_THIRD: process.env.AI_ROUTE_API_THIRD || 'https://api.jayskeralainnovations.com/flask3'
     }
   }]
 };
