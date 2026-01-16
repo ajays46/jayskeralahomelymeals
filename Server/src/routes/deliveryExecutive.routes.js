da90a58a00184b58a0334af86b08a3b078e7f3f3
Vivek <ajay.g@jayskeralaenterprises.com>
2026-01-16 10:16:13 +0530

    changes in multer mb size

--- Server/src/routes/deliveryExecutive.routes.js
+++ Server/src/routes/deliveryExecutive.routes.js
@@ -18,7 +18,7 @@ const router = express.Router();
 const upload = multer({
   storage: multer.memoryStorage(),
   limits: {
-    fileSize: 5 * 1024 * 1024 // 5MB limit
+    fileSize: 10 * 1024 * 1024 // 5MB limit
   },
   fileFilter: function (req, file, cb) {
     if (file.mimetype.startsWith('image/')) {
